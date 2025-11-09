867cba1b0d50caccec0f1b5e10cf1c91744032d7
mordsmiez <212715579+mordsmiez@users.noreply.github.com>
2025-11-09 11:34:04 +0100

    Add code for ComputerChoice&HumanChoice
    Added code in js-file and pseduo code in README

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Computer choice pseudo code:
+
+Get random variable 0,1, or 2
+For 0 return "rock", for 1 return "paper", for 2 return "scissors"
+
+Human chioce pseudo code:
+
+Prompt the player to enter a string
+Store the string for later use
---
+++ rock-paper-scissors.js
@@ -0,0 +1,22 @@
+
+console.log("Hello World")
+
+function getComputerChoice() {
+    let ComputerChoice = Math.floor(Math.random()*3);
+    console.log(ComputerChoice)
+        if (ComputerChoice===0) {
+            return "rock"
+        } else if (ComputerChoice===1) {
+            return "paper"
+        } else {
+            return "scissors"
+        }
+}
+console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let HumanChoice = prompt("Rock, paper or scissors? Which one do you pick?");
+    return HumanChoice
+}
+
+console.log(getHumanChoice())
