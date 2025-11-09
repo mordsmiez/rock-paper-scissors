47686f8d16b517adc4713e68e3e0a9d6ab9a180a
mordsmiez <212715579+mordsmiez@users.noreply.github.com>
2025-11-09 13:04:33 +0100

    Add round function
    Addred round function pseudo code to README and round function code to js-file.

--- README.md
+++ README.md
@@ -8,3 +8,21 @@ Human chioce pseudo code:
 
 Prompt the player to enter a string
 Store the string for later use
+
+Score initialization
+
+Both HumanScore and ComputerScore are 0 in the beginning
+
+Single round pseudo code
+Get Human choice
+Get computer choice
+convert human choice to lower case
+Compare choices
+If computer choice = human choice, return "It's a tie!"
+If Human choice rock and computer choice scissor, or human choice paper and computer choice rock or human choice scissors and computer choice paper, return "You win this round!", increment HumanScore
+If human choice rock and computer choice paper or human choice paper and computer choice scissors or human choice scissors and computer choice rock, return "You lose this round!", increment ComputerScore
+
+Full game pseudo code
+Loop single round function 5 times.
+If player score > computer score, display win message, otherwise display lose message
+Display message with the final score.
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,25 +1,45 @@
 
-console.log("Hello World")
 
 function getComputerChoice() {
-    let ComputerChoice = Math.floor(Math.random()*3);
-    console.log(ComputerChoice)
-        if (ComputerChoice===0) {
+    let randomNumber = Math.floor(Math.random()*3);
+        if (randomNumber===0) {
             return "rock"
-        } else if (ComputerChoice===1) {
+        } else if (randomNumber===1) {
             return "paper"
         } else {
             return "scissors"
         }
 }
-console.log(getComputerChoice())
+
 
 function getHumanChoice() {
-    let HumanChoice = prompt("Rock, paper or scissors? Which one do you pick?");
-    return HumanChoice
+    let humanPick = prompt("Rock, paper or scissors? Which one do you pick?");
+    return humanPick
 }
 
-console.log(getHumanChoice())
 
 let HumanScore = 0;
 let ComputerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice.toLowerCase()===computerChoice) {
+        return "It's a tie! You chose " + humanChoice.toLowerCase() + ". The computer chose " + computerChoice + "."
+    }
+    else if (humanChoice.toLowerCase()==="scissors" && computerChoice==="paper" || humanChoice.toLowerCase()==="rock" && computerChoice==="scissors" || humanChoice.toLowerCase()==="paper" && computerChoice==="rock") {
+        HumanScore++
+        return "You win this round! You chose " + humanChoice.toLowerCase() + ". The computer chose " + computerChoice + "."
+    }
+    else if (humanChoice.toLowerCase()==="paper" && computerChoice==="scissors" || humanChoice.toLowerCase()==="scissors" && computerChoice==="rock" || humanChoice.toLowerCase()==="rock" && computerChoice==="paper") {
+        ComputerScore++
+        return "You lose this round! You chose " + humanChoice.toLowerCase() + ". The computer chose " + computerChoice + "."
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
+console.log(HumanScore)
+console.log(ComputerScore)
